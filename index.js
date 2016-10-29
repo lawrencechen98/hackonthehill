de115e188801a339ac9baac58187e6dadcd35797
Ram <ramsgoli@gmail.com>
2016-10-29 11:00:02 -0700

    set up twitter profile

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: 'jiIijCxDbOnaehbs7b7tqJPyX',
+  consumer_secret: 'DwE2m28SgJgS7lzvY10lKTe35n788d1gJSzx5GoTffgUJ2Bbjl',
+  access_token_key: '792408044491583488-6vQl7QBWCkQoq6gvq0PuKgNQqdoUueb',
+  access_token_secret: 'Zs8gqf6rW341i58Y5fD8gRbzFFGvGR71KkOx1sXW0hFxE'
+});
+
 let app = express();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 app.listen(3000, function() {
 	console.log("listening on port 3000...")
