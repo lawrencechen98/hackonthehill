5c595f815c9dbe39f1eaeb5edd7b27bbe54159c5
Ram <ramsgoli@gmail.com>
2016-10-29 13:01:12 -0700

    getting form data

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,26 +1,36 @@
 var main = function() {
-  $('#ClintonBtn').click(function() {
-    $.ajax({
-    	'url': '/sendClintonTweet',
-    	'type': 'POST',
-    	'success': function(data) {
-    		if (data=="success") {
-    			alert("request sent");
+
+	getTweet: function() {
+		var tweetData = $('.tweet').serializeArray()[0];
+		var tweetText = tweetData[tweet];
+		return tweetText;
+	}
+
+  	$('#ClintonBtn').click(function() {
+
+  		console.log(getTweet());
+  		
+    	$.ajax({
+    		'url': '/sendClintonTweet',
+    		'type': 'POST',
+    		'success': function(data) {
+    			if (data=="success") {
+    				alert("request sent");
+    			}
     		}
-    	}
-    });
-  });
-  $('#TrumpBtn').click(function() {
-    $.ajax({
-    	'url': '/sendTrumpTweet',
-    	'type': 'POST',
-    	'success': function(data) {
-    		if (data=="success") {
-    			alert("request sent");
+    	});
+  	});
+ 	$('#TrumpBtn').click(function() {
+    	$.ajax({
+    		'url': '/sendTrumpTweet',
+    		'type': 'POST',
+    		'success': function(data) {
+    			if (data=="success") {
+    				alert("request sent");
+    			}
     		}
-    	}
-    });
-  });
+    	});
+  	});
 }
 
 $(document).ready(main);
